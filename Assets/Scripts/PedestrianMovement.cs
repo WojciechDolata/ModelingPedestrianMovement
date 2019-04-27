16b12ba8fc3fc8beaf819b86e17ec6606b7ea5e7
WojciechDolata <wojciech.k.dolata@gmail.com>
2019-04-27 15:18:35 +0200

    refactorisation

--- Assets/Scripts/PedestrianMovement.cs
+++ Assets/Scripts/PedestrianMovement.cs
@@ -24,26 +24,26 @@ public class PedestrianMovement : MonoBehaviour
         if (wallCollision) // needed so that they wouldn't slow down near wall
         {
             if (transform.position.y > goal.transform.position.y)
-                transform.position = stepTowards(new Vector3(transform.position.x, transform.position.y - 1f));
+                stepTowards(new Vector3(transform.position.x, transform.position.y - 1f));
             else
-                transform.position = stepTowards(new Vector3(transform.position.x, transform.position.y + 1f));
+                stepTowards(new Vector3(transform.position.x, transform.position.y + 1f));
         }
         else
         {
             if (!isOccupied(tryToStepTowards(goal.transform.position)))
-                transform.position = stepTowards(goal.transform.position);
+                stepTowards(goal.transform.position);
             else
             {
-                int up = 1;
+                int upMultiplier = 1;
                 if (transform.position.y > goal.transform.position.y)
-                    up = -1;
-                float[] arr = { 30f, 45f, 60f, 90f };
-                foreach (float a in arr)
+                    upMultiplier = -1;
+                float[] angles = { 30f, 45f, 60f, 90f };
+                foreach (float currAngle in angles)
                 {
-                    Vector3 tmpGoal = getPositionByAngle(up * a);
+                    Vector3 tmpGoal = getPositionByAngle(upMultiplier * currAngle);
                     if (!isOccupied(tmpGoal))
                     {
-                        transform.position = stepTowards(tmpGoal);
+                        stepTowards(tmpGoal);
                         break;
                     }
                 }
@@ -67,25 +67,21 @@ public class PedestrianMovement : MonoBehaviour
 
     Vector3 getPositionByAngle(float angle) // rotates vector around pivot (self position)
     {
-        Vector3 a = tryToStepTowards(goal.transform.position);
-        Vector3 tmp = tryToStepTowards(goal.transform.position) - transform.position;
-        float ss = tmp.x, b = tmp.y;
-        angle += Mathf.Atan(tmp.y / tmp.x) * (180 / Mathf.PI); //add angle from vector towards goal
-        tmp = Quaternion.Euler(0,0,angle) * tmp;
-        return tmp + transform.position;
+        Vector3 vectorFrom00 = tryToStepTowards(goal.transform.position) - transform.position;
+        angle += Mathf.Atan(vectorFrom00.y / vectorFrom00.x) * (180 / Mathf.PI); //add angle from vector towards goal
+        vectorFrom00 = Quaternion.Euler(0,0,angle) * vectorFrom00;
+        return vectorFrom00 + transform.position;
     }
     bool isOccupied(Vector3 point) // checks if point in space is occupied by any collider2d (excluding self)
     {
-        Collider2D[] tmp = Physics2D.OverlapCircleAll(point, collisionRadius);
-        if (tmp.Length == 0)
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(point, collisionRadius);
+        if (colliders.Length == 0)
             return false;
-        else if (tmp.Length == 1 && tmp[0].transform.position == transform.position)
+        else if (colliders.Length == 1 && colliders[0].transform.position == transform.position)
             return false;
         else
         {
-            var s = tmp[0].ToString();
             return true;
-
         }
     }
 
@@ -93,8 +89,8 @@ public class PedestrianMovement : MonoBehaviour
     {
         return Vector2.MoveTowards(transform.position, direction, 0.5f);
     }
-    Vector3 stepTowards(Vector3 direction) //to get position for actual step
+    void stepTowards(Vector3 direction) //to get position for actual step
     {
-        return Vector2.MoveTowards(transform.position, direction, 0.3f*step);
+        transform.position = Vector2.MoveTowards(transform.position, direction, 0.3f*step);
     }
 }
