7f633b83fcee79f303b77b74c6406e7f3e8bca7d
WiktorWr <wiktor4411@gmail.com>
2019-05-18 17:56:31 +0200

    Add new branch with a 3d project

---
+++ Assets/Manager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public GameObject sampleObject;
+    public float repeatTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("InstantiatePedestrian", 1f, repeatTime);
+    }
+
+    void InstantiatePedestrian()
+    {
+        Vector3 areaSize = objectToSpawn.GetComponent<Renderer>().bounds.size;
+        Vector3 areaToSpawn = new Vector3(
+            Random.Range(objectToSpawn.transform.position.x - areaSize.x / 2, objectToSpawn.transform.position.x + areaSize.x / 2),
+            8f,
+            Random.Range(objectToSpawn.transform.position.z - areaSize.z / 2, objectToSpawn.transform.position.z + areaSize.z / 2));
+        Instantiate(sampleObject, areaToSpawn, Quaternion.identity);
+    }
+}
---
+++ Assets/MoveToGoal.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToGoal : MonoBehaviour
+{
+    public GameObject destinationObject;
+    private Vector3 destinationSize;
+    private Vector3 goalPosition;
+    private NavMeshAgent agent;
+    public float accuracy = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        Vector3 agentSize = this.GetComponent<Renderer>().bounds.size;
+        destinationSize = destinationObject.GetComponent<Renderer>().bounds.size;
+        goalPosition = new Vector3(
+            Random.Range(destinationObject.transform.position.x - destinationSize.x / 2 + agentSize.x, destinationObject.transform.position.x + destinationSize.x / 2 - agentSize.x),
+            8.32f,
+            Random.Range(destinationObject.transform.position.z - destinationSize.z / 2 + agentSize.z, destinationObject.transform.position.z + destinationSize.z / 2 - agentSize.z));
+        agent.SetDestination(goalPosition);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if ((Mathf.Abs(goalPosition.x - this.transform.position.x) < accuracy && Mathf.Abs(goalPosition.z - this.transform.position.z) < accuracy))
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
