105634a1c32bc1a7d5ff6edce020db769a6e95cd
WojciechDolata <wojciech.k.dolata@gmail.com>
2019-05-26 15:34:37 +0200

    Added Menu
    Menu - scene is managed by 2 menu objects and MenuScript.cs (in Assets)

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void RunScene(int number)
+    {
+        SceneManager.LoadScene(number);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
